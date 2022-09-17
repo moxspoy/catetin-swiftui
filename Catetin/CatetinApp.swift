204da33d27abc21025e5bed5159d717498b0c625
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2022-09-17 14:45:59 +0700

    Initial Commit

---
+++ Catetin/CatetinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatetinApp.swift
+//  Catetin
+//
+//  Created by FLP-9-Baehaqi on 17/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CatetinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
