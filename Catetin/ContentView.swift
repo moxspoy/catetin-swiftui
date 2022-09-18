84a8ca6542db954a53a833c4ce9974a52e53d6ad
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2022-09-18 17:44:13 +0700

    separate InputAmountPage

--- Catetin/ContentView.swift
+++ Catetin/ContentView.swift
@@ -8,20 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State public var amount: Int = 0
-    @State public var errorMessage: String = ""
     
     var body: some View {
-        VStack(alignment: .leading) {
-            NavigationBar(title: "Masukkan Jumlah Transfer")
-            ScrollView {
-                AccountCard(account: AccountModel(id: 10, bank: "bca", bankCode: "bca", accountNumber: "9728897288", name: "M Nurilman Baehaqi", accountHolder: "M Nurilman Baehaqi", accountType: "bank_account", status: "SUCCESS"))
-                InputAmountSection(amount: self.$amount)
-                ErrorMessage(message: $errorMessage)
-            }
-            .background(Color("lightGrayBackground"))
-            InputAmountContinueButton(amount: $amount, errorMessage: $errorMessage)
-        }
+        InputAmountPage()
     }
 }
 
